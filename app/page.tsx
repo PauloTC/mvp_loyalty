38af1f21f24cfb7eda98be1a29d2000500fdd23e
Paulo Tejada <paulotejada.123a@gmail.com>
2024-04-19 12:19:25 -0500

    Adding Alicorp components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+"use client";
 import Image from "next/image";
+import { DlInput } from "@alicorpdigital/dali-react";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <p>prueba</p>
+      <DlInput
+        helperText="Helper text"
+        label="Label"
+        placeholder="Placeholder"
+      />
+    </div>
+  );
 }
