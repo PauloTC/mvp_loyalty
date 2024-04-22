f9017cf12fe54cc16f45b4e088919c7048afea36
Paulo Tejada <paulotejada.123a@gmail.com>
2024-04-22 11:57:58 -0500

    adding pages & complete home page

---
+++ app/como-funciona/page.tsx
@@ -0,0 +1,11 @@
+import NestedLayout from "@/components/layout/nestedlayout";
+
+export default async function HowItWorksPage() {
+  return (
+    <NestedLayout>
+      <section>
+        <h4>Mira tu historial</h4>
+      </section>
+    </NestedLayout>
+  );
+}
