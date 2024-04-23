103a2eeb00054412332608a2e131980aa848b613
Paulo Tejada <paulotejada.123a@gmail.com>
2024-04-23 12:01:16 -0500

    update como funciona

--- app/como-funciona/page.tsx
+++ app/como-funciona/page.tsx
@@ -6,10 +6,38 @@ import Image from "next/image";
 export default async function HowItWorksPage() {
   return (
     <NestedLayout>
-      <PrincipalBanner />
+      <ul className=" dl-mb-8 sm:dl-mb-0 dl-flex dl-py-5 dl-container dl-gap-2">
+        <a href="/home" className="dl-flex sm:dl-hidden">
+          <Image
+            alt="arrow_left"
+            width={17}
+            height={14}
+            src="/orders/arrow_left.svg"
+          />
+        </a>
+        <li className="dl-hidden sm:dl-block dl-text-sm">Inicio</li>
+        <Image
+          className="dl-hidden sm:dl-block"
+          alt="breadcrumb_arrow"
+          width={6}
+          height={11}
+          src="/orders/arrow.svg"
+        />
+        <li className="dl-hidden sm:dl-block dl-text-black sm:dl-text-brand-primary-medium dl-font-bold dl-text-sm">
+          Cómo funciona
+        </li>
+        <li className="dl-text-base dl-font-semibold sm:dl-hidden">
+          ¿Cómo funciona?
+        </li>
+      </ul>
+      <div className="dl-hidden sm:dl-block">
+        <PrincipalBanner />
+      </div>
       <section className="dl-container dl-mx-auto">
-        <h4 className="dl-text-2xl dl-font-bold dl-mb-6">¿Cómo funciona?</h4>
-        <ul className="dl-flex dl-justify-between dl-mb-18 dl-gap-8">
+        <h4 className="dl-text-2xl dl-font-bold dl-mb-6">
+          Canjea rápido y fácil
+        </h4>
+        <ul className="dl-flex dl-flex-col md:dl-flex-row dl-justify-between dl-mb-18 dl-gap-8">
           <li>
             <p className="dl-text-sm dl-mb-3">
               1. Acumula puntos comprando desde Insuma fuera del horario de
@@ -43,11 +71,23 @@ export default async function HowItWorksPage() {
                   </div>
                 </div>
               </div>
-
-              <p className="dl-text-sm dl-w-66">
-                Las compras hechas los días que te visita tu vendedor
-                <b>no acumularán puntos</b>
-              </p>
+              <div className="dl-flex">
+                <div
+                  className="dl-rounded-lg dl-w-12 dl-h-10 dl-flex dl-justify-center dl-items-center"
+                  style={{ backgroundColor: "#FFF9F2" }}
+                >
+                  <Image
+                    alt="warning"
+                    width={24}
+                    height={24}
+                    src="/orders/warning.svg"
+                  />
+                </div>
+                <p className="dl-w-48 sm:dl-w-72 dl-text-sm dl-w-66 dl-ml-2">
+                  Las compras hechas los días que te visita tu vendedor
+                  <b>no acumularán puntos</b>
+                </p>
+              </div>
             </div>
           </li>
           <li className="dl-flex dl-flex-col dl-justify-between">
@@ -61,6 +101,16 @@ export default async function HowItWorksPage() {
               src="/orders/empty_order.svg"
             />
           </li>
+          <a
+            href="/canjea-tus-puntos"
+            className="
+            sm:dl-hidden
+            dl-text-white dl-rounded-lg
+            dl-h-12 dl-flex dl-justify-center dl-items-center
+            dl-bg-brand-primary-medium "
+          >
+            Canjear
+          </a>
         </ul>
       </section>
 
