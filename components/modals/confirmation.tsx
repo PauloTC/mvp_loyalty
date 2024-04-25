3b85bba027a42b74f2dc5bf737d9f80038f943c7
iLTW1n <jose.osk01@gmail.com>
2024-04-25 14:06:26 -0500

    feat(g): canjeo puntos modal

--- components/modals/confirmation.tsx
+++ components/modals/confirmation.tsx
@@ -2,9 +2,17 @@
 import { DlModal } from "@alicorpdigital/dali-react";
 import Image from "next/image";
 
-export function ConfirmationModal() {
+type Props = {
+  onClose?: () => void;
+  onOk?: () => void;
+  open?: boolean;
+}
+
+export const ModalConfirmation = (props: Props) => {
+  const { onClose, onOk, open = false } = props;
+
   return (
-    <DlModal open={true}>
+    <DlModal open={open}>
       <div className="dl-flex dl-flex-col dl-items-center">
         <div className="dl-mb-4">
           <Image alt="hands" height={133} width={190} src="/modals/hands.png" />
@@ -18,20 +26,22 @@ export function ConfirmationModal() {
         <div className="dl-flex dl-justify-center dl-mt-8 dl-gap-4">
           <button
             style={{ outline: "none" }}
+            onClick={onClose}
             className="
-            dl-w-44 dl-h-12 
+            dl-w-44 dl-h-12
             dl-flex dl-text-brand-primary-medium
-            dl-justify-center 
+            dl-justify-center
             dl-items-center"
           >
             Cancelar
           </button>
           <button
             style={{ outline: "none" }}
+            onClick={onOk}
             className="
-            dl-w-44 dl-h-12 
-            dl-flex dl-justify-center 
-            dl-items-center 
+            dl-w-44 dl-h-12
+            dl-flex dl-justify-center
+            dl-items-center
             dl-bg-brand-primary-medium dl-text-my-white
             dl-rounded-lg"
           >
