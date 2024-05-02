e02c1335c151098b86cec5cde4f10301ebf38f94
Paulo Tejada <paulotejada.123a@gmail.com>
2024-05-02 12:00:29 -0500

    update pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+      </head>
       <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
