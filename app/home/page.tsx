791b2f2ddf72c919a455ae8984f66e6da73d420e
Paulo Tejada <paulotejada.123a@gmail.com>
2024-05-15 15:06:39 -0500

    update login redirect & card cliked

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -30,31 +30,30 @@ export default function HomePage() {
       <ul className="dl-container dl-mx-auto dl-flex dl-flex-col sm:dl-flex-row  dl-gap-8 sm:dl-gap-20 dl-mb-0 lg:dl-mb-12">
         {actions.map((action, index) => (
           <li key={index} className="dl-grow">
-            <h3 className="dl-text-base dl-font-semibold dl-mb-2">
-              {action.title}
-            </h3>
+            <a href={action.to}>
+              <h3 className="dl-text-base dl-font-semibold dl-mb-2">
+                {action.title}
+              </h3>
 
-            <div className="dl-border dl-rounded-lg">
-              <div className="dl-relative dl-h-26 sm:dl-h-36">
-                <Image
-                  layout="fill"
-                  objectFit="cover"
-                  src={action.image}
-                  alt={action.title}
-                />
+              <div className="dl-border dl-rounded-lg">
+                <div className="dl-relative dl-h-26 sm:dl-h-36">
+                  <Image
+                    layout="fill"
+                    objectFit="cover"
+                    src={action.image}
+                    alt={action.title}
+                  />
+                </div>
+                <div className="dl-border-t d-flex dl-items-center dl-px-4 dl-justify-between dl-h-14">
+                  <p className="dl-font-medium dl-text-sm">
+                    {action.description}
+                  </p>
+                  <span className="dl-text-brand-primary-medium dl-font-semibold">
+                    Ver más
+                  </span>
+                </div>
               </div>
-              <div className="dl-border-t d-flex dl-items-center dl-px-4 dl-justify-between dl-h-14">
-                <p className="dl-font-medium dl-text-sm">
-                  {action.description}
-                </p>
-                <a
-                  href={action.to}
-                  className="dl-text-brand-primary-medium dl-font-semibold"
-                >
-                  Ver más
-                </a>
-              </div>
-            </div>
+            </a>
           </li>
         ))}
       </ul>
