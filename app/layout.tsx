08cab05bc9e2a5a8323ae547411c3577c02a40b4
Paulo Tejada <paulotejada.123a@gmail.com>
2024-05-27 11:44:16 -0500

    update account to experience design

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "mgl1fsng7e");
+            })(window, document, "clarity", "script", "mh48p85jct");
           `}
         </Script>
         <Script
