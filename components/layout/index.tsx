95de5d8b84cd3c78517b44f28f9821963a699d17
iLTW1n <jose.osk01@gmail.com>
2024-07-30 17:26:58 -0500

    feat(g): update points

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,48 +1,13 @@
-import { ReactNode, useContext, useEffect, useState } from "react";
+import { ReactNode } from "react";
 import Footer from "../footer/footer";
 import HeaderComponent from "../header";
-import { AuthContext } from '@/contexts/AuthContext';
-import { SESSION_STORAGE } from '@/constants';
 
 type Props = {
   children: ReactNode;
   hideOnMobile: boolean;
 };
 
-type User = {
-  username: string;
-  password: string;
-  score: number;
-  name?: string;
-  business?: string;
-};
-
 const NestedLayout: React.FC<Props> = ({ children, hideOnMobile }) => {
-  const { user, setUser } = useContext(AuthContext);
-
-  useEffect(() => {
-    fetch("https://my-json-server.typicode.com/paulotc/mvp_loyalty_db/users")
-      .then((response) => response.json())
-      .then((data) => {
-        if (localStorage.getItem("user")) {
-          const userLogin: User | undefined = data.find(
-            (item: User) => item.username === user.username && item.password === user.password
-          );
-          if (userLogin) {
-            const consumed = parseInt(sessionStorage.getItem(SESSION_STORAGE.Consumed) || '0');
-            userLogin.score = userLogin.score - consumed;
-            setUser({
-              username: userLogin.username,
-              score: userLogin.score,
-              name: userLogin.name,
-              business: userLogin.business,
-            });
-            localStorage.setItem("user", JSON.stringify(userLogin));
-          }
-        }
-      });
-  }, []);
-
   return (
     <>
       <HeaderComponent hideOnMobile={hideOnMobile} />
