514991f9cb6d520fc3fde88afaa7c6a7d780014c
iLTW1n <jose.osk01@gmail.com>
2024-07-31 16:27:53 -0500

    feat(g): update points

--- components/my-products/index.tsx
+++ components/my-products/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { DlCardProduct, DlIcon } from '@alicorpdigital/dali-react';
+import cn from 'classnames';
 import React from "react";
 import { ProductProps } from '@/services/products';
 import './styles.css';
@@ -15,32 +16,33 @@ const MyProducts = (props: Props) => {
   const numberWithCommas = (points: number) => {
     return points.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   };
-
   return (
     <>
       {products.map((item, index) => {
         return (
-          <DlCardProduct
-            key={index}
-            src={item.image}
-            title={item.name}
-            className='my-products'
-            inputAmountProps={{
-              value: item.value,
-              onChange: (value) => onChange({ ...item, value }),
-              disabled: true
-            }}
-          >
-            <div className='dl-mt-auto'>
-              <p className='dl-subtitle-xxs'>{numberWithCommas(item.points)}pts</p>
-              {item.unit &&
-                <div className='dl-flex dl-items-center dl-gap-1'>
-                  <DlIcon name='info' color='#E20867' />
-                  <p className='dl-comp-text-quarck'>{item.unit}</p>
-                </div>
-              }
-            </div>
-          </DlCardProduct>
+          <div className={cn({ 'is-disabled': !item.available })}>
+            <DlCardProduct
+              key={index}
+              src={item.image}
+              title={item.name}
+              tag={!item.available ? { content: 'Sin stock', variant: 'pink' } : undefined}
+              inputAmountProps={{
+                value: item.value,
+                onChange: (value) => onChange({ ...item, value }),
+                disabled: true
+              }}
+            >
+              <div className='dl-mt-auto'>
+                <p className='dl-subtitle-xxs'>{numberWithCommas(item.points)}pts</p>
+                {item.unit &&
+                  <div className='dl-flex dl-items-center dl-gap-1'>
+                    <DlIcon name='info' color='#E20867' />
+                    <p className='dl-comp-text-quarck'>{item.unit}</p>
+                  </div>
+                }
+              </div>
+            </DlCardProduct>
+          </div>
         )
       })}
     </>
